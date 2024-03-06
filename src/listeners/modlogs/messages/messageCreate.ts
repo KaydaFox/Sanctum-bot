6541ff240793addffb3421ea2fad797319206821
KaydaFox <lexidahusky.102@gmail.com>
2024-03-06 18:17:00 +0000

    chore: ran eslint

--- src/listeners/modlogs/messages/messageCreate.ts
+++ src/listeners/modlogs/messages/messageCreate.ts
@@ -12,15 +12,15 @@ export class MessageCreateListener extends Listener {
 		if (!message.guild) return;
 		if (message.author.bot || message.author.id === this.container.client.user?.id || message.author.system) return;
 
-    if (message.type !== MessageType.UserJoin && !message.system) return;
+		if (message.type !== MessageType.UserJoin && !message.system) return;
 
 		try {
-      const threadChannel = await message.guild.channels.fetch(envParseString("WELCOMING_CHANNEL_ID"));
-      if(!threadChannel?.isTextBased()) return;
+			const threadChannel = await message.guild.channels.fetch(envParseString('WELCOMING_CHANNEL_ID'));
+			if (!threadChannel?.isTextBased()) return;
 
-      return threadChannel.send({
-        content: `<@&${envParseString("WELCOMING_ROLE_ID")}> ${inlineCodeBlock(message.author.displayName)} has joined the server! Say hello`
-      })
+			return threadChannel.send({
+				content: `<@&${envParseString('WELCOMING_ROLE_ID')}> ${inlineCodeBlock(message.author.displayName)} has joined the server! Say hello`
+			});
 		} catch (error) {
 			return this.container.logger.error(error);
 		}
